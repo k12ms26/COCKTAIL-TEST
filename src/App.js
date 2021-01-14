562098727f5a4898bc8eabfccf7287cb51578f5c
k12ms26 <61970111+k12ms26@users.noreply.github.com>
2021-01-14 16:23:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Questionnaire from './components/Questionnaire';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JavaScript Quiz</h1>
+      <Questionnaire />
+    </div>
+  );
+}
+
+export default App;
