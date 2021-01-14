1992d5580aa71cc8a2433f8e9e75e36c6d3e177d
k12ms26 <meeeeensun@gmail.com>
2021-01-14 20:38:08 +0900

    For Pull

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Questionnaire from './components/Questionnaire';
+import Buttons from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
 
 function App() {
   return (
