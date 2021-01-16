3e11d039ec3518ba1f51a81ff1b86b247d834925
healwon <woni9911@gmail.com>
2021-01-16 10:27:42 +0000

    image get

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs')
+
 const router = express.Router();
 
 var Questions = require('./questions');
@@ -10,6 +12,13 @@ router.use('/result', Results)
 router.use('/index', function(req, res, next) {
     res.json({
         message: 'hello world!'
+    });
+});
+
+router.use('/images/:img', function(req, res, next) {
+    fs.readFile(req.params.img, function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
     });
 });
 
