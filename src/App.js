b37b7083b4e8a42b6e03c33cb76b0ebda0584b3f
healwon <woni9911@gmail.com>
2021-01-17 13:14:56 +0000

    구조 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+import './index.css';
 import Header from './common/header.js'
 import Footer from './common/footer.js'
+import Test from './components/test.js'
+import Result from './components/result.js'
+import Home from './components/home.js'
+import NotFound from './components/notFound.js'
 
 export default class App extends React.Component {
 
@@ -9,7 +16,16 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
-        {this.props.children}
+        <TransitionGroup>
+          <CSSTransition timeout={300} classNames="fade">
+            <Switch>
+              <Route exact path = "/" component = {Home} />
+              <Route path = "/test" component = {Test} />
+              <Route path = "/result" component = {Result} />
+              <Route path = "/*" component = {NotFound} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
--- src/common/header.js
+++ src/common/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../index.css';
-import { Link } from '@version/react-router-v3'
+import { Link } from 'react-router-dom'
+
 export default class Header extends React.Component {
 
   render() {
     return (
       <div className="container">
         <header>
-          <h2><Link to="home" className='link'>Cocktail Test</Link></h2>
+          <h2><Link to="/" className='link'>Cocktail Test</Link></h2>
         </header>
       </div>
     );
