1d338074b983cca5ed55356b1a9481baca6b34f4
healwon <woni9911@gmail.com>
2021-01-17 06:44:20 +0000

    header

--- src/common/footer.js
+++ src/common/footer.js
@@ -5,9 +5,9 @@ export default class Footer extends React.Component {
 
   render() {
     return (
-        <div className="footer">
-          cs496-week3 by 김민선 최예원
-        </div>
+      <footer>
+        cs496-week3 by 김민선 최예원
+      </footer>
     );
   }
 }
